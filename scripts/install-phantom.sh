45c2315cc4f1cc6bd89aa4c2d5c1160a858d10d2
Matthew Radcliffe <mradcliffe@softpixel.com>
2016-02-26 09:48:40 -0500

    Remove condition to install 2.0.0 so that it is always installed

--- scripts/install-phantom.sh
+++ scripts/install-phantom.sh
@@ -1,12 +1,10 @@
 #!/bin/sh
 
-# phantomJS 2.0.0 needs to replace phantom 1.9.x on travis environment.
-# PHANTOM is build variable set in travisci for grunt-crawl.
-if [ "$PHANTOM" = "2.0.0" ]; then
-	mkdir travis-phantomjs
-	wget https://s3.amazonaws.com/travis-phantomjs/phantomjs-2.0.0-ubuntu-12.04.tar.bz2 -O $PWD/travis-phantomjs/phantomjs-2.0.0-ubuntu-12.04.tar.bz2
-	tar -xvf $PWD/travis-phantomjs/phantomjs-2.0.0-ubuntu-12.04.tar.bz2 -C $PWD/travis-phantomjs
-	export PATH=$PWD/travis-phantomjs:$PATH
-fi
+# phantomJS 1.9.8 has a critical bug that is only fixed in 2.0.0, and is thus
+# incompotable with TravisCI.
+mkdir travis-phantomjs
+wget https://s3.amazonaws.com/travis-phantomjs/phantomjs-2.0.0-ubuntu-12.04.tar.bz2 -O $PWD/travis-phantomjs/phantomjs-2.0.0-ubuntu-12.04.tar.bz2
+tar -xvf $PWD/travis-phantomjs/phantomjs-2.0.0-ubuntu-12.04.tar.bz2 -C $PWD/travis-phantomjs
+export PATH=$PWD/travis-phantomjs:$PATH
 
 echo "PhantomJS $PHANTOM installed."
